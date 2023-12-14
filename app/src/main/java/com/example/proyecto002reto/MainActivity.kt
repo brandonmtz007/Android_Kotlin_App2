cbb1c7626eed9a4e84480525601762252ac26f2c
brandonmtz007 <brandonmtz179@gmail.com>
2023-12-14 03:23:47 -0600

    initial commit

---
+++ app/src/main/java/com/example/proyecto002reto/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.proyecto002reto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val et1=findViewById<EditText>(R.id.et1)
+        val et2=findViewById<EditText>(R.id.et2)
+        val tv1=findViewById<TextView>(R.id.tv1)
+        val button=findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            tv1.text = "bienvenido: ${et1.text.toString() +" "+ et2.text.toString()}"
+        }
+
+    }
+}
